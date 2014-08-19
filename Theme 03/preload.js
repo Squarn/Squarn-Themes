c00dd556052572ff40c06e8d521a186534a286bd
Squarn <kotsuzui@gmail.com>
2014-08-19 13:39:31 -0400

    Tweak

---
+++ Theme 03/preload.js
@@ -0,0 +1,7 @@
+// Referrer Credit: BBB on Stackoverflow
+$('document').ready(function(){
+// Makes sure user is coming from fresh session or different URL
+if ( document.referrer == null || document.referrer.indexOf(window.location.hostname) < 0 ) {
+
+}
+});
