97c3780ee7bc7faf62dde492a6e98ed8c75e51c9
Squarn <Kotsuzui@gmail.com>
2014-08-19 07:32:17 -0400

    Signifcant Additions, Added Particleground and BGs

---
+++ Theme 05/script.js
@@ -0,0 +1,61 @@
+	// By Squarn Themes
+	// Causes .tagline and .aside to stick when $window is scrolled
+$(document).ready(function(){
+		// $(function(){
+	 //        var header = $('.top_header').offset().top;
+	 //        //$(".top_header").addClass("sticktop");
+	 //        $(window).scroll(function(){
+  //               if( $(window).scrollTop() < header ) { $('.top_header').addClass('sticktop'); } 
+  //               else { $('.top_header').removeClass('sticktop'); }
+	 //        });
+		// });
+	
+		$(function(){
+			$('article').mouseenter(
+				function(){$('.meta', this).stop().slideDown("slow","easeOutBounce");}
+			);
+			$('article').mouseleave(
+				function(){$('.meta', this).stop().slideUp(600,"easeInBack");}
+			);	
+		});
+
+		$(function(){
+	        var tagline = $('.tagline').offset().top;
+	          
+	        $(window).scroll(function(){
+                if( $(window).scrollTop() > tagline ) { $('.tagline').addClass('stickt'); } 
+                else { $('.tagline').removeClass('stickt'); }
+
+                if( $(window).scrollTop() > tagline ) { $('footer').fadeIn("fast"); } 
+                else { $('footer').hide(); }
+
+                if($('.tagline').hasClass("stickt"))
+                {
+                	$('.pagn').fadeIn();
+                	$('.pagp').fadeIn();
+                }
+                else
+                {
+                	$('.pagn').hide();
+                	$('.pagp').hide();
+                }
+	        });
+		});
+
+		$(function(){
+	        var aside = $('aside').offset().top - 50;
+	          
+	        $(window).scroll(function(){
+                if( $(window).scrollTop() > aside ) {
+                        $('aside').addClass('sticka');
+                } else {
+                        $('aside').removeClass('sticka');
+                }
+	        });
+		});
+
+		$( ".nav" ).hover(
+			function() {$(this).stop().animate({opacity: 0.5, paddingLeft: "10",}, 1000, "easeOutBounce", function() {});},
+			function() {$(this).stop().animate({opacity: 1, paddingLeft: "0",}, 1000, "easeOutBounce", function() {});}
+		);
+});
