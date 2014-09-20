08392daf3a5b1b949545f48b7064e2bca400b648
Squarn <kotsuzui@gmail.com>
2014-09-20 17:03:36 -0400

    Cleaned up T5 Header and Nav

--- Theme 05/script.js
+++ Theme 05/script.js
@@ -1,78 +1,92 @@
 $(document).ready(function(){
-// By Squarn Themes
+	// By Squarn Themes
+		
+		// Particleground load
+		$('#particles').particleground({
+            minSpeedX: 0.1,
+            maxSpeedX: 0.1,
+            minSpeedY: 0.1,
+            maxSpeedY: 0.1,
+            directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+            directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+            density: 10000, // How many particles will be generated: one particle every n pixels
+            dotColor: '#3d3d3d',
+            lineColor: '#666666',
+            particleRadius: 10, // Dot size
+            lineWidth: 0.5,
+            curvedLines: false,
+            proximity: 120, // How close two dots need to be before they join
+            parallax: true,
+            parallaxMultiplier: 8, // The lower the number, the more extreme the parallax effect
+            onInit: function() { console.log("Particles initiated properly."); },
+            onDestroy: function() {}
+        });
 
-	$('#particles').particleground({
-		minSpeedX: 0.1,
-		maxSpeedX: 0.1,
-		minSpeedY: 0.1,
-		maxSpeedY: 0.1,
-		directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
-		directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
-		density: 10000, // How many particles will be generated: one particle every n pixels
-		dotColor: '#3d3d3d',
-		lineColor: '#666666',
-		particleRadius: 10, // Dot size
-		lineWidth: .5,
-		curvedLines: false,
-		proximity: 120, // How close two dots need to be before they join
-		parallax: true,
-		parallaxMultiplier: 8, // The lower the number, the more extreme the parallax effect
-		onInit: function() {},
-		onDestroy: function() {}
-	});
+		// Meta slide down
+		$(function(){
+			$('article').mouseenter(
+				function(){
+					$('.meta', this).stop().slideDown(1000,"easeOutBounce");
+					//$(this).css("marginBottom",0);
+				}
+			);
+			$('article').mouseleave(
+				function(){
+					$('.meta', this).stop().slideUp(300,"linear");
+					//$(this).css("marginBottom",30);
+				}
+			);	
+		});
 
-	$(function(){
-		$('article').mouseenter(
-			function(){
-				$('.meta', this).stop().slideDown("slow","easeOutBounce");
-				$(this).css("marginBottom",0);
-			}
-		);
-		$('article').mouseleave(
-			function(){
-				$('.meta', this).stop().slideUp(600,"easeInBack");
-				$(this).css("marginBottom",30);
-			}
-		);	
-	});
-
-	$(function(){
 		// Tagline Stick
-		var tagline = $('.tagline').offset().top - 10;
-		  
-		$(window).scroll(function(){
-			if( $(window).scrollTop() > tagline ) { $('.tagline').addClass('stickt'); } 
-			else { $('.tagline').removeClass('stickt'); }
+		$(function(){
+			var tagline = $('.tagline').offset().top - 5;
 
-			// Pagination Fade In
-			if($('.tagline').hasClass("stickt"))
-			{
-				$('.pagn').fadeIn();
-				$('.pagp').fadeIn();
-			}
-			else
-			{
-				$('.pagn').hide();
-				$('.pagp').hide();
-			}
+			$(window).scroll(function(){
+				if( $(window).scrollTop() > tagline ) { $('.tagline').addClass('stickt'); } 
+				else { $('.tagline').removeClass('stickt'); }
+
+				// Pagination Fade In
+				if($('.tagline').hasClass("stickt"))
+				{
+					$('.pagn').fadeIn();
+					$('.pagp').fadeIn();
+				}
+				else
+				{
+					$('.pagn').hide();
+					$('.pagp').hide();
+				}
+			});
 		});
-	});
 
-	$(function(){
-		// Sidebar Stick
-		var aside = $('aside').offset().top - 50;
-		  
-		$(window).scroll(function(){
-			if( $(window).scrollTop() > aside ) {
-				$('aside').addClass('sticka');
-			} else {
-				$('aside').removeClass('sticka');
-			}
+		$(function(){
+			// Sidebar Stick
+			var aside = $('aside').offset().top - 50;
+
+			$(window).scroll(function(){
+				if( $(window).scrollTop() > aside ) {
+					$('aside').addClass('sticka');
+				} else {
+					$('aside').removeClass('sticka');
+				}
+			});
 		});
-	});
 
-	$( ".nav" ).hover(
-		function() {$(this).stop().animate({opacity: 0.5, paddingLeft: "10",}, 1000, "easeOutBounce", function() {});},
-		function() {$(this).stop().animate({opacity: 1, paddingLeft: "0",}, 1000, "easeOutBounce", function() {});}
-	);
+		$( ".nav" ).hover(
+			function() {$(this).stop().animate({opacity: 0.5, paddingLeft: "10"}, 300, "easeOutBounce", function() {});},
+			function() {$(this).stop().animate({opacity: 1, paddingLeft: "0"}, 300, "easeOutBounce", function() {});}
+		);
+
+		if ( document.referrer === null || document.referrer.indexOf(window.location.hostname) < 0 ) 
+		{
+			
+		}
+
+		else {$('#particles').particlegound('destroy');}
+
+		$(".poop").click(function() {
+			$(".postbody").fadeOut();
+			$(".help").fadeIn();
+		});
 });
